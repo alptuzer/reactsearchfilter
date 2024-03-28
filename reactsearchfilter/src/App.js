42ba1e68087624801d7b549fc46b354296645a38
alp <alptuzer@gmail.com>
2024-03-28 18:36:33 +0300

    2

--- reactsearchfilter/src/App.js
+++ reactsearchfilter/src/App.js
@@ -1,14 +1,46 @@
-import React, { Component } from 'react';
- 
+import React, { Component, useState } from 'react';
+import data from './datas/data001.json';
+import "./style/app.css"
 
-class App extends Component {
-  render() {
+
+function  App()  {
+  
+  const [arama,setArama] = useState(data);
+  const [searchTerm,setSearchTerm] = useState("");
+
     return (
       <div className="App">
+
+        <div>
+        <h1> Ürünler</h1>
+        <div className='inputContainer'>
+          <input className='search' type="text" placeholder='Arama Ürrünü Giriniz' onChange={(e)=>setSearchTerm(e.target.value)}></input>
+        </div>
+
+        <div className='dataContainer'>
+          {data.filter((val)=> {
+            if(searchTerm ==""){
+              return val;
+            }
+            else if (val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+            {return val;}
+          }).map((val)=>{
+            return (
+              <div className='data' key={val.id}>
+                <img src="./pic/leaf.jpg"></img>
+                <h3> {val.image}</h3>
+                <h3> {val.title}</h3>
+                <p>{val.Description}</p>
+                </div>
+            )
+          })
+          }
+        </div>
+        </div>
   
       </div>
     );
   }
-}
+ 
 
 export default App;
